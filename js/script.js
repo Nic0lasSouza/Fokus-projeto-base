d443e660531461a515ef7d4d51a5536d22e5d0a6
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-05-10 19:31:15 -0300

    slterando as imagens com click

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-const html = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longBt = document.querySelector('.app__card-button--longo')
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', ()=>{ //adiconaod evento de click
-    html.setAttribute('data-contexto', 'foco') // atribuir um atributo
+    html.setAttribute('data-contexto', 'foco') ;// atribuir um atributo
+    banner.setAttribute('src', '/img/foco.png')
 })
 
 curtoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/img/descanso-curto.png')
+})
+
+longBt.addEventListener('click', ()=> {
+    html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/img/descanso-longo.png')
 })
